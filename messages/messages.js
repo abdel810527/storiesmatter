f2e3637b9cd8da8b3c4c6834cd641326cdf08dc9
abdel810527 <32907447+abdel810527@users.noreply.github.com>
2020-09-20 15:04:41 +0200

    Add files via upload

---
+++ messages/messages.js
@@ -0,0 +1,7 @@
+module.exports = {
+  personalStories: "Mijn persoonlijke verhalen",
+  publicStories: "Gedeelde verhalen",
+  deletePost: "Verhaal verwijderd",
+  storySaved: "Verhaal bewaard",
+  storyEdited: "Verhaal bewerkt"
+};
